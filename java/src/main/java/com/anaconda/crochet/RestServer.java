6f12f682fc3571df1fd31b26bd551bc6f19a856e
Jim Crist <jiminy.crist@gmail.com>
2018-03-28 00:36:49 -0500

    Serve content on two ports
    For now one port is authenticated and the other isn't. In the future,
    one port will use the secret key, while the other will go through
    kerberos and only serve a subset of the content.

--- java/src/main/java/com/anaconda/crochet/RestServer.java
+++ java/src/main/java/com/anaconda/crochet/RestServer.java
@@ -7,6 +7,7 @@ import javax.servlet.DispatcherType;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -15,36 +16,66 @@ import org.eclipse.jetty.servlet.ServletHolder;
 public class RestServer {
     private static final EnumSet<DispatcherType> REQUEST_SCOPE = EnumSet.of(DispatcherType.REQUEST);
 
+    private static String secret;
+
     private static final ConcurrentHashMap<String, byte[]> configuration
         = new ConcurrentHashMap<String,byte[]>();
 
-    public static Integer port = -1;
+    private static Integer privatePort = -1;
+    private static Integer publicPort = -1;
 
     public static void main(String[] args) throws Exception {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        String secret = System.getenv("CROCHET_SECRET_ACCESS_KEY");
+        secret = System.getenv("CROCHET_SECRET_ACCESS_KEY");
+
         if (secret == null) {
             System.err.println("Couldn't find secret token at " +
                                "'CROCHET_SECRET_ACCESS_KEY' envar");
             System.exit(1);
         }
 
-        context.setContextPath("/");
-        FilterHolder holder = context.addFilter(AuthenticationFilter.class,
-                                                "/*", REQUEST_SCOPE);
+        // Configure the server
+        Server server = new Server();
+        HandlerCollection handlers = new HandlerCollection();
+        server.setHandler(handlers);
+
+        // Create the servlets once
+        ServletHolder keyVal = new ServletHolder(new KeyValueServlet(configuration));
+
+        // This connector serves content authenticated by the secret key
+        ServerConnector privateConnector = new ServerConnector(server);
+        privateConnector.setPort(8080);
+        privateConnector.setName("Private");
+        server.addConnector(privateConnector);
+
+        ServletContextHandler privateContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        privateContext.setContextPath("/");
+        privateContext.setVirtualHosts(new String[]{"@Private"});
+        privateContext.addServlet(keyVal, "/keys/*");
+        FilterHolder holder = privateContext.addFilter(AuthenticationFilter.class,
+                                                       "/*", REQUEST_SCOPE);
         holder.setInitParameter("secret", secret);
+        handlers.addHandler(privateContext);
 
-        // Add the key-value store servlet
-        context.addServlet(new ServletHolder(new KeyValueServlet(configuration)), "/keys/*");
+        // This connector serves content unauthenticated
+        ServerConnector publicConnector = new ServerConnector(server);
+        publicConnector.setPort(8081);
+        publicConnector.setName("Public");
+        server.addConnector(publicConnector);
 
-        // Start the server
-        Server server = new Server(8080);
-        server.setHandler(context);
+        ServletContextHandler publicContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        publicContext.setContextPath("/");
+        publicContext.setVirtualHosts(new String[]{"@Public"});
+        publicContext.addServlet(keyVal, "/keys/*");
+        handlers.addHandler(publicContext);
+
+        // Startup the server
         server.start();
 
-        // Determine port used
-        port = ((ServerConnector)server.getConnectors()[0]).getLocalPort();
-        System.out.println("Listening at localhost:" + port);
+        // Determine ports
+        privatePort = privateConnector.getLocalPort();
+        publicPort = publicConnector.getLocalPort();
+        System.out.println("Private access at localhost:" + privatePort);
+        System.out.println("Public access at localhost:" + publicPort);
 
         // Run until closing
         server.join();
