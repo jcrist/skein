5806b097d428d54805ea0bacfe02c4f7a7425192
Jim Crist <jcrist@users.noreply.github.com>
2018-05-16 19:04:44 -0500

    Build docs with doctr (#2)
    * Build docs with doctr
    
    * Tweaks
    
    * Only build on master
    
    - Only build pushes to master branch or PRs
    - Only deploy docs on master

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -1,6 +1,6 @@
 set -xe
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh -nv
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
--- continuous_integration/travis/script.sh
+++ continuous_integration/travis/script.sh
@@ -4,3 +4,12 @@ set -xe
 py.test skein/ -vv
 
 flake8 skein/
+
+if [[ "$DOCS" == "true" ]]; then
+    pushd docs/
+    make html
+    popd
+    if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_EVENT_TYPE" == "push" ]]; then
+        doctr deploy . --built-docs docs/build/html/
+    fi
+fi
