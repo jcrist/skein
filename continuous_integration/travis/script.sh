903eccde359a6b942978b9a3496d411bcbf4c786
Jim Crist <jiminy.crist@gmail.com>
2018-05-16 19:01:07 -0500

    Only build on master
    - Only build pushes to master branch or PRs
    - Only deploy docs on master

--- continuous_integration/travis/script.sh
+++ continuous_integration/travis/script.sh
@@ -9,5 +9,7 @@ if [[ "$DOCS" == "true" ]]; then
     pushd docs/
     make html
     popd
-    doctr deploy . --built-docs docs/build/html/
+    if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_EVENT_TYPE" == "push" ]]; then
+        doctr deploy . --built-docs docs/build/html/
+    fi
 fi
