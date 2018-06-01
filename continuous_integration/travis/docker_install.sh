4b4dbe013bd07342e51c5cfad9cb4b6179f9611e
Jim Crist <jiminy.crist@gmail.com>
2018-06-01 15:51:53 -0700

    Maybe now?

--- continuous_integration/travis/docker_install.sh
+++ continuous_integration/travis/docker_install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -xe
 
-conda install grpcio pyyaml cryptography pytest
+conda install grpcio pyyaml cryptography pytest flake8
 
-cd ~/workdir
+pip install grpcio-tools
+
+# Copy and chown the built files, due to docker permissions issue
+cp -r ~/workdir ~/skein
+chown -R testuser:testuser /home/testuser/skein
+
+cd ~/skein
 pip install -e .
 
 conda list
