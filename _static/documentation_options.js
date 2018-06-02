319d0142113449ea6df5931e018a346004a7c2ef
Travis CI User <travis@example.org>
2018-06-02 00:04:00 +0000

    Update docs after building Travis build 55 of jcrist/skein
    The docs were built from the branch 'master' against the commit
    4673b57d94fc6e4ac7fb032e0f44f4cb53917741.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/skein/jobs/386927757.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.0.2+20.ge0b49ee',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.0.2+21.g4673b57',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
