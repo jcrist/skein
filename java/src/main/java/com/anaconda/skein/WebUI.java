e792f84ddc9e93f1a3897380782cfab7746b77e1
Jim Crist <jiminy.crist@gmail.com>
2018-09-06 13:05:56 -0500

    Working prototype

--- java/src/main/java/com/anaconda/skein/WebUI.java
+++ java/src/main/java/com/anaconda/skein/WebUI.java
@@ -10,18 +10,22 @@ import com.github.mustachejava.util.DecoratedCollection;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.rewrite.handler.RedirectPatternRule;
+import org.eclipse.jetty.rewrite.handler.RewriteHandler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -38,18 +42,32 @@ public class WebUI {
   public void configure(int port,
                         String appId,
                         Map<String, Msg.KeyValue.Builder> keyValueStore,
-                        Map<String, ServiceContext> services)
+                        List<ServiceContext> services)
       throws Exception {
 
     server = new Server(port);
+
+    // Handler for all static resources
+    ServletContextHandler context = new ServletContextHandler();
+    context.setContextPath("/");
+    // Hack to get directory containing resources, since `URI.resolve` doesn't
+    // work on opaque (e.g. non-filesystem) paths.
+    URI baseURI = URI.create(
+        WebUI.class.getResource("/META-INF/resources/favicon.ico").toURI()
+                   .toASCIIString().replaceFirst("/favicon.ico$","/")
+    );
+    LOG.info("Serving Resources From: " + baseURI);
+    context.setBaseResource(Resource.newResource(baseURI));
+    context.addServlet(new ServletHolder("default", DefaultServlet.class), "/");
+
+    // Issue a 302 redirect to services from homepage
+    RewriteHandler rewrite = new RewriteHandler();
+    rewrite.addRule(new RedirectPatternRule("", "/services"));
+
     server.setHandler(new HandlerList(
+        rewrite,
         new WebApiHandler(appId, keyValueStore, services),
-        new ResourceHandler() {
-          {
-            setDirectoriesListed(false);
-            setResourceBase(WebUI.class.getResource("/META-INF/resources").toExternalForm());
-          }
-        },
+        context,
         new DefaultHandler()));
   }
 
@@ -80,34 +98,36 @@ public class WebUI {
     kv.put("Key 3", Msg.KeyValue.newBuilder()
                        .setValue(ByteString.copyFrom(new byte[] {(byte) 255})));
     // Create a dummy services map
-    Map<String, ServiceContext> services = new HashMap<String, ServiceContext>();
+    String url = "https://dummyurl.html";
+    List<ServiceContext> services = Lists.newArrayList();
     ServiceContext service1 = new ServiceContext();
     service1.name = "Service 1";
     service1.running = 3;
     service1.succeeded = 1;
-    service1.stopped = 1;
+    service1.killed = 1;
     service1.failed = 1;
     service1.active = Lists.newArrayList(
-        new ContainerInfo(1, 0, 1, Model.Container.State.WAITING, ""),
-        new ContainerInfo(2, 0, 1, Model.Container.State.REQUESTED, ""),
-        new ContainerInfo(4, 0, 1, Model.Container.State.RUNNING, "https://dummyurl.html")
+        new ContainerInfo(1, 0, 0, Model.Container.State.WAITING, ""),
+        new ContainerInfo(2, 0, 0, Model.Container.State.REQUESTED, ""),
+        new ContainerInfo(4, 0, 3 * 60 + 24, Model.Container.State.RUNNING, url)
     );
     service1.completed = Lists.newArrayList(
-        new ContainerInfo(0, 0, 1, Model.Container.State.SUCCEEDED, "https://dummyurl.html"),
-        new ContainerInfo(3, 0, 1, Model.Container.State.KILLED, "https://dummyurl.html"),
-        new ContainerInfo(5, 0, 1, Model.Container.State.FAILED, "https://dummyurl.html")
+        new ContainerInfo(0, 0, 30, Model.Container.State.SUCCEEDED, url),
+        new ContainerInfo(3, 0, 90, Model.Container.State.KILLED, url),
+        new ContainerInfo(5, 0, 60 * 60 * 2 + 90, Model.Container.State.FAILED, url)
     );
-    services.put("Service 1", service1);
+    services.add(service1);
     ServiceContext service2 = new ServiceContext();
     service2.name = "Service 2";
     service2.running = 1;
     service2.succeeded = 0;
-    service2.stopped = 0;
+    service2.killed = 0;
     service2.failed = 0;
     service2.active = Lists.newArrayList(
-        new ContainerInfo(0, 0, 1, Model.Container.State.RUNNING, "https://dummyurl.html")
+        new ContainerInfo(0, 0, 24, Model.Container.State.RUNNING, url)
     );
-    services.put("Service 2", service2);
+    service2.completed = Lists.newArrayList();
+    services.add(service2);
 
     try {
       webui.configure(port, "application_1526497750451_0001", kv, services);
@@ -133,13 +153,42 @@ public class WebUI {
       this.status = status;
       this.logAddress = logAddress;
     }
+
+    public String runtime() {
+      long delta;
+      switch (status) {
+        case WAITING:
+        case REQUESTED:
+          return "0s";
+        case RUNNING:
+          delta = System.currentTimeMillis() - startTime;
+          break;
+        default:
+          delta = finishTime - startTime;
+      }
+      long secs = delta / 1000;
+      secs = finishTime - startTime;
+      long hours = secs / (60 * 60);
+      secs = secs % (60 * 60);
+      long mins = secs / 60;
+      secs = secs % 60;
+
+      if (hours > 0) {
+        return String.format("%dh %dm", hours, mins);
+      }
+      else if (mins > 0) {
+        return String.format("%dm %ds", mins, secs);
+      } else {
+        return String.format("%ds", secs);
+      }
+    }
   }
 
   public static class ServiceContext {
     public String name;
     public int running;
     public int succeeded;
-    public int stopped;
+    public int killed;
     public int failed;
     public List<ContainerInfo> active;
     public List<ContainerInfo> completed;
@@ -149,12 +198,12 @@ public class WebUI {
 
   private static class Context {
     private final String appId;
-    private final Map<String, ServiceContext> services;
+    private final List<ServiceContext> services;
     private final Map<String, Msg.KeyValue.Builder> keyValueStore;
 
     public Context(String appId,
                    Map<String, Msg.KeyValue.Builder> keyValueStore,
-                   Map<String, ServiceContext> services) {
+                   List<ServiceContext> services) {
       this.appId = appId;
       this.keyValueStore = keyValueStore;
       this.services = services;
@@ -162,7 +211,7 @@ public class WebUI {
 
     public String appId() { return appId; }
 
-    public DecoratedCollection<Map.Entry<String, String>> kv() {
+    public List<Map.Entry<String, String>> kv() {
       synchronized (keyValueStore) {
         List<Map.Entry<String, String>> out =
             Lists.newArrayListWithCapacity(keyValueStore.size());
@@ -173,20 +222,18 @@ public class WebUI {
                                       ? value.toStringUtf8()
                                       : "<binary value>"));
         }
-        return new DecoratedCollection<Map.Entry<String, String>>(out);
+        return out;
       }
     }
 
-    public DecoratedCollection<Map.Entry<String, ServiceContext>> services() {
-      List<Map.Entry<String, ServiceContext>> entries =
-          Lists.newArrayList(services.entrySet());
-      Collections.sort(entries, new Comparator<Map.Entry<String, ServiceContext>>() {
-        public int compare(Map.Entry<String, ServiceContext> e1,
-                            Map.Entry<String, ServiceContext> e2) {
-          return e1.getKey().compareTo(e2.getKey());
+    public DecoratedCollection<ServiceContext> services() {
+      List<ServiceContext> entries = Lists.newArrayList(services);
+      Collections.sort(entries, new Comparator<ServiceContext>() {
+        public int compare(ServiceContext e1, ServiceContext e2) {
+          return e1.name.compareTo(e2.name);
         }
       });
-      return new DecoratedCollection<Map.Entry<String, ServiceContext>>(entries);
+      return new DecoratedCollection<ServiceContext>(entries);
     }
   }
 
@@ -201,7 +248,7 @@ public class WebUI {
 
     public WebApiHandler(String appId,
                          Map<String, Msg.KeyValue.Builder> keyValueStore,
-                         Map<String, ServiceContext> services) {
+                         List<ServiceContext> services) {
       this.context = new Context(appId, keyValueStore, services);
     }
 
@@ -209,6 +256,7 @@ public class WebUI {
                        HttpServletRequest request,
                        HttpServletResponse response)
         throws IOException {
+
       if (baseRequest.isHandled() || !HttpMethod.GET.is(request.getMethod())) {
         return;
       }
