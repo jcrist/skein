5b15b23c05d6bbd1faa47ea76c9ec98080c33c0a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-02 16:40:22 +0000

    Update docs after building Travis build 328 of jcrist/skein
    The docs were built from the branch 'master' against the commit
    41888be097f95a86a3eaed0b0fb7c99cd6845116.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/skein/jobs/514773112.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.0+1.ga80b895',
+    VERSION: '0.6.0+2.g41888be',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
