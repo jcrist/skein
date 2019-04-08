8a3856ce336e60d34973abe519ecf97e24147684
Jim Crist <jiminy.crist@gmail.com>
2019-04-08 14:29:01 -0500

    Add missing classes to docs

--- skein/__init__.py
+++ skein/__init__.py
@@ -4,7 +4,7 @@ from .exceptions import (SkeinError, ConnectionError, DriverNotRunningError,
                          ApplicationNotRunningError, DriverError,
                          ApplicationError)
 from .model import (Security, ApplicationSpec, Service, File, Resources,
-                    FileType, FileVisibility, ACLs, Master)
+                    FileType, FileVisibility, ACLs, Master, LogLevel)
 
 from ._version import get_versions
 __version__ = get_versions()['version']
