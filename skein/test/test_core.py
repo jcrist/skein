ab5e56e4a17512d2eee83563002ce9b7b7171772
Jim Crist <jiminy.crist@gmail.com>
2019-05-01 14:58:41 -0500

    Fix shutdown condition
    In the absence of restarts, a failing container should cause the whole
    application to fail. However, the code implementing this was broken.
    This fixes that and adds tests to enforce the specified behavior.

--- skein/test/test_core.py
+++ skein/test/test_core.py
@@ -1083,6 +1083,36 @@ def test_allow_failures_max_restarts(client, allow_failures):
             assert wait_for_completion(client, app.id) == 'FAILED'
 
 
+@pytest.mark.parametrize('with_restarts', [True, False])
+def test_fail_on_container_failure(client, with_restarts):
+    script = ('if [[ "$SKEIN_CONTAINER_ID" != "test_0" ]]; then\n'
+              '  exit 1\n'
+              'else\n'
+              '  sleep infinity\n'
+              'fi')
+
+    spec = skein.ApplicationSpec(
+        name="test_fail_on_container_failure",
+        services={
+            'test': skein.Service(
+                instances=2,
+                max_restarts=2 if with_restarts else 0,
+                resources=skein.Resources(memory=32, vcores=1),
+                script=script
+            )
+        }
+    )
+    with run_application(client, spec=spec) as app:
+        wait_for_completion(client, app.id) == "FAILED"
+
+    logs = get_logs(app.id)
+    assert "test_0" in logs
+    assert "test_1" in logs
+    assert ("test_2" in logs) == with_restarts
+    assert ("test_3" in logs) == with_restarts
+    assert "test_4" not in logs
+
+
 def test_move_application(client):
     spec = skein.ApplicationSpec(
         name="test_move_application",
