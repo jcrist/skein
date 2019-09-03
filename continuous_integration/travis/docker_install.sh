67f6beb9d613d1c8e09cae07816ceba26cbf15e6
Jim Crist <jiminy.crist@gmail.com>
2019-09-03 16:02:58 -0500

    Update travis dependencies

--- continuous_integration/travis/docker_install.sh
+++ continuous_integration/travis/docker_install.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 set -xe
 
-packages="grpcio pyyaml cryptography pytest flake8 requests pyarrow nomkl"
-
-if [[ $1 == "2.7" ]]; then
-    conda create -n py27 python=2.7 $packages
-    source activate py27
-else
-    conda install $packages
-fi
+conda install \
+    grpcio \
+    pyyaml \
+    cryptography \
+    pytest \
+    flake8 \
+    requests \
+    pyarrow \
+    nomkl \
+    pykerberos \
+    requests-kerberos \
+    tornado
 
 pip install grpcio-tools
 
