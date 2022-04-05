62c39571c0890ee5f8c0b6459faed371d3bd37ff
Jim Crist-Harif <jcristharif@gmail.com>
2022-04-05 10:59:49 -0500

    Update skein/core.py

--- skein/core.py
+++ skein/core.py
@@ -213,10 +213,9 @@ def _start_driver(security=None, set_global=False, keytab=None, principal=None,
     env = dict(os.environ)
     env['SKEIN_CERTIFICATE'] = security._get_bytes('cert')
     env['SKEIN_KEY'] = security._get_bytes('key')
-    # Construct a CLASSPATH env var by this package's bundled skein.jar with
-    # hadoops yarn's configured classpath.
-    yarn_detected_classpath = subprocess.check_output(['yarn', 'classpath', '--glob']).decode('utf-8').strip()
-    env['CLASSPATH'] = '%s:%s' % (_SKEIN_JAR, yarn_detected_classpath)
+    # Construct classpath from skein.jar & hadoop default classpath
+    yarn_classpath = subprocess.check_output(['yarn', 'classpath', '--glob']).decode('utf-8').strip()
+    env['CLASSPATH'] = '%s:%s' % (_SKEIN_JAR, yarn_classpath)
 
     callback = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     callback.bind(('127.0.0.1', 0))
